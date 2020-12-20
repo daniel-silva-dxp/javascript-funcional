b6f4ea09ffc992465e553c53b657597cfd5901bc
Daniel Silva <dani3l.dxp@gmail.com>
2020-12-20 12:19:25 -0300

    feat: iniciando com funções

---
+++ funcoes/funcao_#1.js
@@ -0,0 +1,11 @@
+// Declaração de função
+function foo(msg) {
+  console.log(msg);
+}
+foo("Olá");
+
+// Expressão de função
+const bar = function (msg) {
+  console.log(msg);
+};
+bar("Daniel Silva");
