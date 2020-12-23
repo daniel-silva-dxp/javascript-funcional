ab5112d885295c87e97a0ad8c853cea65f083d45
Daniel Silva <dani3l.dxp@gmail.com>
2020-12-23 18:12:40 -0300

    feat: resto da divisão

---
+++ exercicios/#4.js
@@ -0,0 +1,7 @@
+/**
+ * Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+ * e o resto da divisão destes dois valores
+ */
+
+const resto = (a, b) => a % b;
+console.log(resto(10, 3));
