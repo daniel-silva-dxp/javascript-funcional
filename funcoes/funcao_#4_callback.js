3bd56396273b16e0c5bebddf021fefbe5a31b756
Daniel Silva <dani3l.dxp@gmail.com>
2020-12-23 15:23:19 -0300

    feat: callbaack

--- funcoes/funcao_#4_callback.js
+++ funcoes/funcao_#4_callback.js
@@ -6,12 +6,12 @@ const dir = path.join(__dirname, "data.txt");
 const viewData = (_, content) => {
   console.log(content.toString());
 };
-fs.readFile(dir, {}, viewData);
+//fs.readFile(dir, {}, viewData);
 
 const fabricantes = ["Mercedes", "Audi", "BMW"];
 
 const imprimir = (nome, indice) => console.log(`${indice + 1}: ${nome}`);
-fabricantes.forEach(imprimir);
+//fabricantes.forEach(imprimir);
 
 // Sem Callback
 const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0];
@@ -23,8 +23,37 @@ for (let nota of notas) {
   }
 }
 
-console.log(desaprovados);
+//console.log(desaprovados);
 
 // Com Callback
 const desaprovadosII = notas.filter((nota) => nota < 7);
-console.log(desaprovadosII);
+//console.log(desaprovadosII);
+
+const rand = (min = 1000, max = 3000) => {
+  const n = Math.random() * (max - min) + min;
+  return Math.floor(n);
+};
+
+const fnI = (cb) => {
+  setTimeout(() => {
+    console.log("fn I");
+    if (cb) cb();
+  }, rand(100, 3500));
+};
+
+const fnII = (cb) => {
+  setTimeout(() => {
+    console.log("fn II");
+    if (cb) cb();
+  }, rand(100, 2500));
+};
+
+const fnIII = (cb) => {
+  setTimeout(() => {
+    console.log("fn III");
+    if (cb) cb();
+  }, rand(100, 3000));
+};
+
+// Callback hell
+fnI(() => fnII(() => fnIII(() => console.log("Fim callbacks"))));
