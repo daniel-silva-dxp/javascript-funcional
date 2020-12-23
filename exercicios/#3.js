5cefea21748396108b8c2f024002e2d30c570581
Daniel Silva <dani3l.dxp@gmail.com>
2020-12-23 18:04:20 -0300

    feat: cálculo de base elevado ao expoente

---
+++ exercicios/#3.js
@@ -0,0 +1,6 @@
+/**
+ * Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+ */
+
+const potencia = (b, e) => Math.pow(b, e);
+console.log(potencia(2, 8));
