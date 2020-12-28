d5437b4339757ad847ac7407a874f91219e30d53
Daniel Silva <dani3l.dxp@gmail.com>
2020-12-28 19:04:42 -0300

    feat: Array.map() #3

--- funcoes/map_#1.js
+++ funcoes/map_#1.js
@@ -11,3 +11,20 @@ console.log(nomes.sort());
 notas.map((value, index, arr) =>
   console.log(`Value: ${value}, Index: ${index}, Arrey: ${arr}`)
 );
+
+const carrinho = [
+  { nome: "Caneta", qtde: 10, preco: 7.99 },
+  { nome: "Impressora", qtde: 0, preco: 649.5 },
+  { nome: "Caderno", qtde: 4, preco: 27.1 },
+  { nome: "Lápis", qtde: 3, preco: 5.82 },
+  { nome: "Tesoura", qtde: 1, preco: 19.2 },
+];
+
+const getNome = (element) => element.nome;
+console.log(carrinho.map(getNome));
+
+const setTotal = (element) => {
+  element.total = element.preco * element.qtde;
+  return element;
+};
+console.log(carrinho.map(setTotal));
