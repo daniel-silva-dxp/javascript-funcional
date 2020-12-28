e0c00a81eae02bec258b51dfa863bf6c7a99cea7
Daniel Silva <dani3l.dxp@gmail.com>
2020-12-28 18:17:16 -0300

    feat: Array.map() #2

--- funcoes/map_#1.js
+++ funcoes/map_#1.js
@@ -1,11 +1,13 @@
 const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0];
-const numeros = [2, 3, 5, 10, 1, 11, 25, 36, 45];
 const notasArredondadas = notas.map((nota) => Math.round(nota));
+console.log(notasArredondadas);
 
-console.log(
-  notasArredondadas,
-  numeros.sort((a, b) => a - b)
-);
+const numeros = [2, 3, 5, 10, 1, 11, 25, 36, 45];
+console.log(numeros.sort((a, b) => a - b));
 
 const nomes = ["Daniel", "Matheus", "Flávia", "Ana"];
 console.log(nomes.sort());
+
+notas.map((value, index, arr) =>
+  console.log(`Value: ${value}, Index: ${index}, Arrey: ${arr}`)
+);
