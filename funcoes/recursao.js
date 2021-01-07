768eb3ca15f5eeafee92ff5b7ed26fa91ae002b0
Daniel Silva <dani3l.dxp@gmail.com>
2021-01-07 14:18:51 -0300

    feat: simulando o Array.map() com recursão

--- funcoes/recursao.js
+++ funcoes/recursao.js
@@ -6,3 +6,26 @@ const sum = (a) => {
 };
 
 console.log(sum(arr));
+
+// Array.prototype.map() recursivo
+const cart = [
+  { nome: "Caneta", qtde: 10, preco: 7.99 },
+  { nome: "Impressora", qtde: 0, preco: 649.5 },
+  { nome: "Caderno", qtde: 4, preco: 27.1 },
+  { nome: "Lápis", qtde: 3, preco: 5.82 },
+  { nome: "Tesoura", qtde: 1, preco: 19.2 },
+];
+
+const getName = (element) => element.nome;
+
+const myMap = (a = [], fn = (item) => item) => {
+  return (function fnInternal(arr, counter) {
+    const [head, ...tail] = arr;
+    return arr.length === 0
+      ? []
+      : [fn(head, counter, a), ...fnInternal(tail, counter + 1)];
+  })(a, 0);
+};
+
+const productNames = myMap(cart, getName);
+console.log(productNames);
