bbc18228cf50d51954b9bbb0e222eaddadfef699
Daniel Silva <dani3l.dxp@gmail.com>
2021-01-12 10:17:38 -0300

    feat: finalizando a implementação de uma função que simule o Array.reduce()

--- funcoes/reduce_#1.js
+++ funcoes/reduce_#1.js
@@ -16,14 +16,37 @@ const counterNames = (allNames, name) => {
 
 console.log(names.reduce(counterNames, {}));
 
+const arrNumbers = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+const reducer = arrNumbers.reduce((acc, a) => acc.concat(a));
+console.log(reducer);
+
+let newArr = [1, 2, 1, 2, 3, 5, 4, 5, 3, 4, 4, 4, 4];
+const result = newArr.sort().reduce((init, current) => {
+  if (init.length === 0 || init[init.length - 1] !== current)
+    init.push(current);
+
+  return init;
+}, []);
+
+console.log(newArr, result);
 // Implementando função que simula o Array.reduce()
 
 const myReduce = (arr, fn, initial) => {
   let acc = initial;
-  arr.forEach((el, i, a) => {
+  const a = arr;
+  if (initial === undefined) {
+    acc = arr[0];
+    a = arr.slice(1);
+  }
+  a.forEach((el, i, a) => {
     acc = fn(acc, el, i, a);
   });
   return acc;
 };
 
-console.log("my reduce", myReduce(numbers, sum, 0));
+console.log(myReduce(numbers, sum, 0));
