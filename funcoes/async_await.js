0645d12b9596a28903d659e02f2cca244905e721
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-01 19:49:20 -0300

    feat: entendendo async/await - tratamento de erro

--- funcoes/async_await.js
+++ funcoes/async_await.js
@@ -12,8 +12,36 @@ const foo = (msg, time) => {
 
 async function exec() {
   console.log("I");
-  const dataReturn = await foo("Primeiro", randomTime(1, 3)).then(console.log);
+  const dataReturn = await foo("Primeiro", randomTime(1, 3));
   console.log("F");
 }
 
-exec();
+// exec().then(console.log);
+
+const generateNumbersBetween = (min = 1, max = 5, repeatedNumbers = []) => {
+  if (min > max) [max, min] = [min, max];
+
+  return new Promise((resolve, reject) => {
+    const result = parseInt(Math.random() * (max - min + 1) + min);
+    repeatedNumbers.includes(result)
+      ? reject("Repeated Number")
+      : resolve(result);
+  });
+};
+
+async function megaSena(quantityOfNumbers = 5, counter = 1) {
+  try {
+    const arr = [];
+    for (let _ of Array(quantityOfNumbers).fill()) {
+      arr.push(await generateNumbersBetween(1, 60, arr));
+    }
+
+    return arr;
+  } catch (e) {
+    if (counter > 5) throw "Exceeded attempt limit";
+
+    return megaSena(quantityOfNumbers, counter + 1);
+  }
+}
+
+megaSena(5).then(console.log).catch(console.log);
