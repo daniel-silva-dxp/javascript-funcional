87bcaa02a501e8cb336e2ce5614424782c6a50fc
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-02 17:08:02 -0300

    feat: separando o texto por linhas

--- projeto/index.js
+++ projeto/index.js
@@ -6,4 +6,6 @@ const dir = path.join(__dirname, "data", "subtitle");
 fn.readDirectory(dir)
   .then((files) => fn.filterReadData(files, ".srt"))
   .then((fileSrt) => fn.readFiles(fileSrt))
+  .then((content) => content.join("\n"))
+  .then((str) => str.split("\n"))
   .then(console.log);
