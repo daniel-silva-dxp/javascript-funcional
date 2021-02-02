70ae0d5379d6ff0423bd1c032d048e4aebe7cc2b
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-02 16:51:06 -0300

    feat: extraindo arquivos indesejados

--- projeto/app.js
+++ projeto/app.js
@@ -2,10 +2,21 @@ const fs = require("fs");
 const path = require("path");
 
 function readDirectory(dir) {
-  let files = fs.readdirSync(dir);
-  return files.map((file) => path.join(dir, file));
+  return new Promise((resolve, reject) => {
+    try {
+      let files = fs.readdirSync(dir);
+      resolve(files.map((file) => path.join(dir, file)));
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+function filterReadData(arr, extension) {
+  return arr.filter((element) => element.endsWith(extension));
 }
 
 module.exports = {
   readDirectory,
+  filterReadData,
 };
